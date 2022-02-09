b3e51632fae75a50a299a5f6b859e11e4b4180df
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-09 11:32:44 -0500

    Add project files.

---
+++ BankingLibraryProject/Account.cs
@@ -0,0 +1,21 @@
+﻿using AccountFormatLibrary;
+
+using System;
+
+namespace BankingLibraryProject {
+    
+    public class Account {
+        // Properties
+        public int AccountNumber { get; set; } = 1;
+        public string Description { get; set; } = "Interest-bearing Account";
+        public decimal Balance { get; set; } = 100m; // $100
+        public decimal InterestRate { get; set; } = 0.12m; // 12% interest rate
+        // Method to Calculate how much Interest to be paid using FormatAccount.cs
+        public decimal CalculateInterest(int months) {
+            decimal interestAmount
+                = FormatAccount.CalculateInterestByMonths(months,InterestRate,Balance);
+            Balance += interestAmount;
+            return interestAmount;
+        }
+    }
+}
